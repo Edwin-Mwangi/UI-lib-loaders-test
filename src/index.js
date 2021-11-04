57756add7e62f0f5468f6f9362f9e1275afa48d2
Edwin Mwangi <edwinmwangi138@gmail.com>
2021-11-05 01:27:02 +0300

    Dropdowns and ingernoius '>' use

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import { Tooltip } from "./ninja-ui/tooltip";
+import Dropdown from "./ninja-ui/dropdown";
 
 //create a tooltip
 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 
 tooltip.init();//"I am a tooltip is now output to the DOM"
+
+//dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown =>{
+    const instance = new Dropdown(dropdown);
+    instance.init();
+});
