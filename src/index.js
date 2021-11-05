162f44df00eb5dccd814bb9cbd0ecf9cd7828a7e
Edwin Mwangi <edwinmwangi138@gmail.com>
2021-11-05 03:05:04 +0300

    Snackbars

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { Tooltip } from "./ninja-ui/tooltip";
 import Dropdown from "./ninja-ui/dropdown";
 import Tabs from "./ninja-ui/tabs";
-
+import Snackbar from "./ninja-ui/snackbar";
 //create a tooltip
 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
@@ -16,5 +16,15 @@ dropdowns.forEach(dropdown =>{
     instance.init();
 });
 
+//tabs
 const tabs = new Tabs(document.querySelector('.tabs')); 
 tabs.init();
+
+//snackbar
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () =>{
+    snackbar.show('You clicked me :)');
+})
